6674b4f13ae52bb73cc185b227479befcc70e17a
originbenntou <originbenntou8973@gmail.com>
2021-10-23 20:27:17 +0900

    save

--- src/app.rs
+++ src/app.rs
@@ -3,11 +3,22 @@ use crate::events::EventState;
 use crate::config::Config;
 use std::fs;
 
-use tui::widgets::ListState;
+use tui::{
+    Frame,
+    backend::CrosstermBackend,
+    backend::Backend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{
+        Block, BorderType, Borders, List, ListItem, ListState, Tabs,
+    },
+    Terminal,
+};
 
 pub struct App<'a> {
     config: &'a Config<'a>,
-    list_state: ListStates<'a>,
+    list_state: ListStates,
     address_list: &'a Vec<&'a str>,
     count_list: &'a Vec<&'a str>,
 }
@@ -15,16 +26,107 @@ pub struct App<'a> {
 impl<'a> App<'a> {
     pub fn new(
         config: &'a Config<'a>,
-        list_state: ListStates<'a>,
         address_list: &'a Vec<&'a str>,
         count_list: &'a Vec<&'a str>
     ) -> Self {
+        // From選択構造体
+        let mut from_list_state = ListState::default();
+        from_list_state.select(Some(0));
+
+        // Count選択構造体
+        let mut count_list_state = ListState::default();
+        count_list_state.select(Some(0));
+
+        let list_state = ListStates::new(
+            from_list_state,
+            count_list_state
+        );
+
         Self {
             config,
             list_state,
             address_list,
             count_list
         }
+    }
+
+    pub fn draw<B: Backend>(&mut self, f: &mut Frame<'_, B>) {
+        // 縦方向分割
+        let vertical_chunk = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(0)
+            .constraints(
+                [
+                    Constraint::Length(3),
+                    Constraint::Min(2),
+                ]
+                    .as_ref(),
+            )
+            .split(f.size());
+
+        let menu = vec!["Add", "Delete", "Execute", "Quit"]
+            .iter()
+            .map(|t| {
+                let (first, rest) = t.split_at(1);
+                Spans::from(vec![
+                    Span::styled(
+                        first,
+                        Style::default()
+                            .fg(Color::Yellow)
+                            .add_modifier(Modifier::UNDERLINED),
+                    ),
+                    Span::styled(rest, Style::default().fg(Color::White)),
+                ])
+            })
+            .collect();
+
+        // 上部メニュー
+        let tabs = Tabs::new(menu)
+            .block(Block::default().title("Menu").borders(Borders::ALL))
+            .style(Style::default().fg(Color::White))
+            .divider(Span::raw("|"));
+        f.render_widget(tabs, vertical_chunk[0]);
+
+        // 横方向分割
+        let horizon_chunk = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(
+                [
+                    Constraint::Percentage(47),
+                    Constraint::Percentage(6),
+                    Constraint::Percentage(47),
+                ].as_ref(),
+            )
+            .split(vertical_chunk[1]);
+
+        // 左部Fromリスト
+        let left = render_froms(
+            "From",
+            self.address_list.clone(), // ループするクロージャごとでmoveされるため
+        );
+        f.render_stateful_widget(left, horizon_chunk[0], &mut self.list_state.from);
+
+        let mid = render_froms(
+            "Count",
+            self.count_list.clone(), // ループするクロージャごとでmoveされるため
+        );
+        f.render_stateful_widget(mid, horizon_chunk[1], &mut self.list_state.count);
+
+        let content = fs::read_to_string(self.config.mark_list_path).unwrap();
+
+        // if db_content == "" {
+        //     let empty_vec: Vec<String> = Vec::new();
+        //     Ok((empty_vec))
+        // }
+
+        let mark_list: Vec<String> = serde_json::from_str(&content).unwrap();
+
+        // 右部Targetリスト
+        let right = render_froms(
+            "Target",
+            mark_list.iter().map(AsRef::as_ref).collect()
+        );
+        f.render_widget(right, horizon_chunk[2]);
     }
 
     pub fn event(&mut self, key: KeyCode) -> Result<EventState, ()> {
@@ -115,16 +217,44 @@ impl<'a> App<'a> {
     }
 }
 
-pub struct ListStates<'a> {
-    from: &'a mut ListState,
-    count: &'a mut ListState,
+pub struct ListStates {
+    from: ListState,
+    count: ListState,
 }
 
-impl<'a> ListStates<'a> {
-    pub fn new(from: &'a mut ListState, count: &'a mut ListState) -> Self {
+impl ListStates {
+    pub fn new(from: ListState, count: ListState) -> Self {
         Self {
             from,
             count,
         }
     }
 }
+
+fn render_froms<'a>(block_name: &'a str, from_list: Vec<&'a str>) -> List<'a> {
+    let from_block = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::White))
+        .title(block_name)
+        .border_type(BorderType::Plain);
+
+    let items: Vec<_> = from_list
+        .iter()
+        .map(|from| {
+            ListItem::new(Spans::from(vec![Span::styled(
+                from.clone(),
+                Style::default(),
+            )]))
+        })
+        .collect();
+
+    let list = List::new(items).block(from_block).highlight_style(
+        Style::default()
+            .bg(Color::Yellow)
+            .fg(Color::Black)
+            .add_modifier(Modifier::BOLD),
+    );
+
+    list
+}
+
--- src/main.rs
+++ src/main.rs
@@ -87,22 +87,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
 
-    let menu_titles = vec!["Add", "Delete", "Execute", "Quit"];
-
-    // From選択構造体
-    let mut from_list_state = ListState::default();
-    from_list_state.select(Some(0));
-
-    // Count選択構造体
-    let mut count_list_state = ListState::default();
-    count_list_state.select(Some(0));
-
     let mut app = App::new(
         &config,
-        ListStates::new(
-            &mut from_list_state,
-            &mut count_list_state
-        ),
         &address_list,
         &count_list,
     );
@@ -112,74 +98,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         // widget生成
         terminal.draw(|f| {
-            // 縦方向分割
-            let vertical_chunk = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(0)
-                .constraints(
-                    [
-                        Constraint::Length(3),
-                        Constraint::Min(2),
-                    ]
-                        .as_ref(),
-                )
-                .split(f.size());
-
-            let menu = menu_titles
-                .iter()
-                .map(|t| {
-                    let (first, rest) = t.split_at(1);
-                    Spans::from(vec![
-                        Span::styled(
-                            first,
-                            Style::default()
-                                .fg(Color::Yellow)
-                                .add_modifier(Modifier::UNDERLINED),
-                        ),
-                        Span::styled(rest, Style::default().fg(Color::White)),
-                    ])
-                })
-                .collect();
-
-            // 上部メニュー
-            let tabs = Tabs::new(menu)
-                .block(Block::default().title("Menu").borders(Borders::ALL))
-                .style(Style::default().fg(Color::White))
-                .divider(Span::raw("|"));
-            f.render_widget(tabs, vertical_chunk[0]);
-
-            // 横方向分割
-            let horizon_chunk = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints(
-                    [
-                        Constraint::Percentage(47),
-                        Constraint::Percentage(6),
-                        Constraint::Percentage(47),
-                    ].as_ref(),
-                )
-                .split(vertical_chunk[1]);
-
-            // 左部Fromリスト
-            let left = render_froms(
-                "From",
-                address_list.clone(), // ループするクロージャごとでmoveされるため
-            );
-            f.render_stateful_widget(left, horizon_chunk[0], &mut from_list_state);
-
-            let mid = render_froms(
-                "Count",
-                count_list.clone(), // ループするクロージャごとでmoveされるため
-            );
-            f.render_stateful_widget(mid, horizon_chunk[1], &mut count_list_state);
-
-            // 右部Targetリスト
-            let mark_list = read_db().unwrap();
-            let right = render_froms(
-                "Target",
-                mark_list.iter().map(AsRef::as_ref).collect()
-            );
-            f.render_widget(right, horizon_chunk[2]);
+            app.draw(f);
         })?;
 
         match events.next()? {
@@ -205,43 +124,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     terminal.show_cursor()?;
 
     Ok(())
-}
-
-fn render_froms<'a>(block_name: &'a str, from_list: Vec<&'a str>) -> List<'a> {
-    let from_block = Block::default()
-        .borders(Borders::ALL)
-        .style(Style::default().fg(Color::White))
-        .title(block_name)
-        .border_type(BorderType::Plain);
-
-    let items: Vec<_> = from_list
-        .iter()
-        .map(|from| {
-            ListItem::new(Spans::from(vec![Span::styled(
-                from.clone(),
-                Style::default(),
-            )]))
-        })
-        .collect();
-
-    let list = List::new(items).block(from_block).highlight_style(
-        Style::default()
-            .bg(Color::Yellow)
-            .fg(Color::Black)
-            .add_modifier(Modifier::BOLD),
-    );
-
-    list
-}
-
-fn read_db() -> Result<Vec<String>, Error> {
-    let content = fs::read_to_string(MARK_LIST_PATH)?;
-
-    // if db_content == "" {
-    //     let empty_vec: Vec<String> = Vec::new();
-    //     Ok((empty_vec))
-    // }
-
-    let parsed: Vec<String> = serde_json::from_str(&content)?;
-    Ok(parsed)
 }
